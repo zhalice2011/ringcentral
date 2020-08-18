32a916006496c30407ab9724ca82bee9805dbc51
zhalice2011 <zhalice2012@gmail.com>
2020-08-19 01:09:29 +0800

    v1.0

---
+++ public/precache-manifest.8f101e6883aa593ccaf62ccb3a91fd5a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0bdb7d4953ed575c2a9abf0ca79a95e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9948d4321431db1857c3",
+    "url": "/static/css/main.b4f35d18.chunk.css"
+  },
+  {
+    "revision": "7efd0af9f2b639fe1ec2",
+    "url": "/static/js/2.aad2feda.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aad2feda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9948d4321431db1857c3",
+    "url": "/static/js/main.10106c00.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "1e8f226267fbda8389236b70db9b6635",
+    "url": "/static/media/iconfont.1e8f2262.eot"
+  },
+  {
+    "revision": "2af4a57c3cf54bad08f0e9edc4dcd500",
+    "url": "/static/media/iconfont.2af4a57c.woff"
+  },
+  {
+    "revision": "8788016840eddc376e6821d7f672b934",
+    "url": "/static/media/iconfont.87880168.ttf"
+  },
+  {
+    "revision": "d2f66bb39814973469374683201a2f05",
+    "url": "/static/media/iconfont.d2f66bb3.svg"
+  }
+]);
